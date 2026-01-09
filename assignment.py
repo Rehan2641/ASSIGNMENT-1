3bc956860e123a3ea692c1493cf0f8fd2ea6ab0b
REHAN AHMAD <rehan8750156416@gmail.com>
2026-01-09 14:50:25 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,50 @@
+#1.  Takes two numbers as input from the user.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+#2.  Performs the basic mathematical operations on these two numbers:
+
+# Addition
+Addition = a+b
+
+#Subtraction
+Subtraction = a-b
+
+# Multiplication
+Multiplication = a*b
+
+# Division
+Division = a/b
+
+
+# 3.  Displays the results of each operation on the screen.
+
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+
+# Task 2: Create a Personalized Greeting
+
+# 1.  Takes a user's first name and last name as input.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#2.Concatenates the first name and last name into a full name.
+
+full_name = (first_name+' '+last_name)
+
+# 3.  Prints a personalized greeting message using the full name.
+
+print(f"Hello, {full_name}! welcome to the Python program.")
+
+
+
+
+
+
+
+
+
